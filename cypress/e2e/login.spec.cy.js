fbc05404d3d150474a525228c55d62100e4bd895
Simone Hirosse <simonehirosse.dev@gmail.com>
2024-04-19 14:21:32 -0300

    Primeiro commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,8 @@
+describe('Orange HRM Tests', () => {
+  it('Login - Success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+  })
+})
